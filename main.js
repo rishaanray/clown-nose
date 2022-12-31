da38e6d9f02cc2a1744edb10c0b11049b0244f4d
rishaanray <85553353+rishaanray@users.noreply.github.com>
2022-12-31 10:51:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+nosex=00;
+nosey=00;
+
+function preload(){
+    clown_nose=loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+function setup(){
+ canvas= createCanvas( 300, 300);
+ canvas.center();
+video= createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+console.log('Posenet is initialized');
+}
+function gotPoses(results){
+if( results.length > 0){
+    console.log(results);
+    nosex=results[0].pose.nose.x-5;
+    nosey=results[0].pose.nose.y-5;
+}
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, nosex, nosey, 30, 30);
+}
+function takeSnapshot(){
+    save('myfilterimage.png');
+}
